aebc0ab91b6edba22265fdf35838c20842f3cc6b
Ahamad-Tanzeel-Khan <ahamadtanzeel08@gmail.com>
2023-11-09 18:19:15 +0530

    deleted updates

--- config/lowerSearch.js
+++
@@ -1,8 +0,0 @@
-// api/middleware/lowercaseSearch.js
-module.exports = async (ctx, next) => {
-    if (ctx.request.url.startsWith('/api/products') && ctx.request.query['filters[title][$contains]']) {
-      ctx.request.query['filters[title][$contains]'] = ctx.request.query['filters[title][$contains]'].toLowerCase();
-    }
-    await next();
-  };
-  
--- config/middlewares.js
+++ config/middlewares.js
@@ -22,5 +22,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'strapi::lowercaseSearch',
 ];
